f70d0a041240072b63d4450931189d1b5df8a8af
Da Fú <74943984+Arkadian404@users.noreply.github.com>
2024-11-11 21:09:41 +0700

    first commit

---
+++ src/main/java/org/zafu/spring_new/SpringNewApplication.java
@@ -0,0 +1,13 @@
+package org.zafu.spring_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zafu/spring_new/SpringNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zafu.spring_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
